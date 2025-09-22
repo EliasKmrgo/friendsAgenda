6fabe49d6fc0cebcaef97721e3542797e0c09b7a
Arian Selene Daza Ochoa <arian.daza@uptc.edu.co>
2025-09-22 11:11:59 -0500

    sqlfiles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,3 @@
-
-CREATE TABLE person (
-    person_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    age INT CHECK (age >= 0),
-    email VARCHAR(150) UNIQUE NOT NULL
-);
-
 INSERT INTO person (username, name, age, email) VALUES
 ('juan123', 'Juan Pérez', 28, 'juan@example.com'),
 ('sofia99', 'Sofía Gómez', 26, 'sofia@example.com'),
